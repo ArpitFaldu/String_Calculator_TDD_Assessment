e2dd5fbf2af46761ea48be2af028da04c846e6f4
Faldu Arpit <127038757+ArpitFaldu@users.noreply.github.comfalduarpit2004@gmail.com>
2025-07-05 15:04:36 +0530

    test: added test case for empty string

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -1,4 +1,13 @@
+const add=require('../src/StringCalculator')
 
 test('Jest is working!', () => {
     expect(1).toBe(1);
 });
+
+describe('Started writing tests',()=>{
+    
+    test('Return 0 for empty string', () => {
+        expect(add("")).toBe(0)
+    });
+
+})
